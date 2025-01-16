8370a1d62c601f3955fba781dd12a9f57d9fdcd8
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-01-16 10:11:16 +0900

    ユーザーのログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-    #名前でログインできるようにViewのセッションNewとレジストレーションNewのEmailのところをいじった
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時にnameパラメータを許可
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name]) # アカウント更新時にnameパラメータを許可
+    end
+  end
